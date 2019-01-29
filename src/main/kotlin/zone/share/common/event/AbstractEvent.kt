bd174378c0866ab740eaf9c22ebff42bf27fe073
e-sen <i@iamee.me>
2019-01-29 18:16:25 +0800

    增加seq,time字段

--- src/main/kotlin/zone/share/common/event/AbstractEvent.kt
+++ src/main/kotlin/zone/share/common/event/AbstractEvent.kt
@@ -7,4 +7,19 @@ import zone.share.common.Identifiable
  * @author Iamee
  * @create 2019-01-29 17:33
  */
-abstract class AbstractEvent : Identifiable
+abstract class AbstractEvent : Identifiable {
+
+    lateinit var seq: String
+        private set
+
+    var time: Long = 0
+        private set
+
+    constructor()
+
+    constructor(seq: String, time: Long) {
+        this.seq = seq
+        this.time = time
+    }
+
+}
